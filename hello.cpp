0df2d1db93d234f6c93e7059267b6579f89d52a7
Aditya Prakash <prakashaditya25957@gmail.com>
2023-07-06 20:00:03 +0530

    dsfwsf

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,47 @@
 #include<bits/stdc++.h>
 #include<math.h>
 using namespace std;
-typedef unsigned long long int ll;
+typedef long long int ll;
 
 int main(){
-    int x = 99999;
-    cout << x;
+    #ifndef ONLINE_JUDGE
+    freopen("error.txt", "w", stderr);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    ll n; scanf("%lld",&n);
+    ll k; scanf("%lld",&k);
+    ll arr[n];
+    for (ll i = 0; i < n; i++)
+    {
+        scanf("%lld",&arr[i]);
+    }
+    ll maximum,max_pos,help;
+    if(k>=n){
+        sort(arr,arr+n,greater<int>());
+    }else{
+        for (ll i = 0; i < k ; i++)
+        {
+            maximum = *max_element(arr+i, arr + n);
+            // max_pos = distance(arr, max_element(arr+i, arr + n));
+            // printf("max = %d ,max_pos = %d\n",maximum,max_pos);
+            if(arr[i] != maximum){
+                help = arr[i];
+                arr[i] = maximum;
+                arr[distance(arr, max_element(arr+i, arr + n))] = help;
+                // swap1(mid,max_pos);
+            }else{
+                k++;
+                continue;
+            }
+
+            // cout << "\n";
+        }
+    }
+    for (ll i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    
     return 0;    
 }
--- largestPermutation.cpp
+++ largestPermutation.cpp
@@ -1,29 +1,48 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+#include<math.h>
 using namespace std;
-typedef vector<int> vec;
+typedef long long int ll;
 
-int main(int argc, char const *argv[])
-{
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("error.txt", "w", stderr);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
     int n; scanf("%d",&n);
     int k; scanf("%d",&k);
-    vec arr;
+    int arr[n];
     for (int i = 0; i < n; i++)
     {
         scanf("%d",&arr[i]);
     }
-    int count = 0;
-    for (int i = 0; i < n; i++)
-    {
-        if(arr[i] !=  *max_element(arr.begin() + i,arr.end()) && count < k){
-            swap(arr[i],*max_element(arr.begin(),arr.end()));
-            count++;
+    int maximum,max_pos,help;
+    if(k>=n){
+        sort(arr, arr + n, greater<int>());
+    }else{
+        for (int i = 0; i < k ; i++)
+        {
+            maximum = *max_element(arr+i, arr + n);
+            max_pos = distance(arr, max_element(arr+i, arr + n));
+            // printf("max = %d ,max_pos = %d\n",maximum,max_pos);
+            if(arr[i] == maximum){
+                if(k+1<=n){k++;}
+                continue;
+            }
+            if(arr[i] != maximum){
+                help = arr[i];
+                arr[i] = maximum;
+                arr[max_pos] = help;
+                // swap1(mid,max_pos);
+            }
+            // cout << "\n";
         }
     }
+
     for (int i = 0; i < n; i++)
     {
         printf("%d ",arr[i]);
     }
     
     return 0;
-
 }
